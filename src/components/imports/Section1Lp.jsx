94b2a1b1af57cfab0d8a6e1c6faa647bb6d214e3
mircomania <mirco.rodriguez@hotmail.com>
2025-02-06 20:58:28 -0300

    4 tamir commit - section 1 lp 1.1

--- src/components/imports/Section1Lp.jsx
+++ src/components/imports/Section1Lp.jsx
@@ -1,9 +1,23 @@
+import { NavLink } from 'react-router-dom';
+
 export const Section1Lp = () => {
     return (
         <section className="section-1-lp-container">
-            <div className="section-1-titulo">
+            {/* Fondos animados */}
+            <div className="bg bg-1"></div>
+            <div className="bg bg-2"></div>
+            <div className="bg bg-3"></div>
+
+            {/* Contenido */}
+            <div className="section-1-lp-titulo">
                 <h1 className="pacifico-text">Sesión de Fotos</h1>
-                <h2 className="light-text-montserrat">Para bebes, embazaradas y toda tu familia</h2>
+                <h2 className="light-text-montserrat">Para bebés, embarazadas y toda tu familia</h2>
+            </div>
+
+            <div className="section-1-lp-boton">
+                <NavLink to="/contacto" aria-label="Ir a la página de Contacto">
+                    <button className="boton-1 bold-text-montserrat">AGENDAR</button>
+                </NavLink>
             </div>
         </section>
     );
