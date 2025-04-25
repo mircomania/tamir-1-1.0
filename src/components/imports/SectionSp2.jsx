8ec74e513ae72fa5a3451e4908348b6e0464fc31
mircomania <mirco.rodriguez@hotmail.com>
2025-04-25 18:13:11 -0400

    31 tamir commit - section 2 servicios page

--- src/components/imports/SectionSp2.jsx
+++ src/components/imports/SectionSp2.jsx
@@ -1,11 +1,43 @@
 import styles from '../../styles/modulos/sectionSp2.module.css';
 
+import { NavLink } from 'react-router-dom';
+
 export const SectionSp2 = () => {
     return (
         <section className={styles.sectionContainer}>
             <header className={styles.sectionTitulo}>
-                <h2 className="bold-text-montserrat">hola</h2>
+                <h2 className="bold-text-montserrat">Instrucciones Generales</h2>
             </header>
+
+            <div className={styles.sectionContenido}>
+                <ul className="light-text-montserrat">
+                    <li>
+                        Para reservar una sesión se debe abonar un <strong>porcentaje del valor de plan </strong> como abono inicial.
+                    </li>
+
+                    <li>Si no hay un estilo de vestimenta acordado, utiliza ropa con colores neutros y sin estampados llamativos.</li>
+
+                    <li>
+                        Las sesiones pueden durar entre <strong>45 min y 2 horas</strong>, dependiendo del plan seleccionado.
+                    </li>
+
+                    <li>
+                        Llegar al menos <strong>10 minutos antes</strong> para preparativos y briefing.
+                    </li>
+
+                    <li>Trae accesorios o props personales si deseas incluirlos en las fotografías.</li>
+
+                    <li>En caso de menores de edad, asegúrate de que un adulto responsable esté presente durante toda la sesión.</li>
+                </ul>
+
+                <p className="light-text-montserrat">
+                    Para más detalles o consultas, visita nuestras{' '}
+                    <NavLink to="/faq" title="ir a las preguntas frecuentes">
+                        Preguntas Frecuentes
+                    </NavLink>{' '}
+                    o escribenos a <strong>contacto@tamir.cl</strong>
+                </p>
+            </div>
         </section>
     );
 };
--- src/components/pages/ServiciosPage.jsx
+++ src/components/pages/ServiciosPage.jsx
@@ -1,5 +1,6 @@
 import { SectionSp1 } from '../imports/SectionSp1';
 import { SectionSp2 } from '../imports/SectionSp2';
+import { BotonNav } from '../utils/BotonNav';
 
 const ServiciosPage = () => {
     return (
@@ -10,6 +11,10 @@ const ServiciosPage = () => {
 
             <section>
                 <SectionSp2 />
+            </section>
+
+            <section className="boton-container-2">
+                <BotonNav />
             </section>
         </main>
     );
