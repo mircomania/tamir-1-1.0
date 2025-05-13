a803df8cdaf480e1bd46cfd30bb9869af828b0d8
mircomania <mirco.rodriguez@hotmail.com>
2025-05-13 18:12:16 -0400

    39 tamir commit - full imagenes section 3 LP

--- src/components/utils/categoriasSectionLp3.js
+++ src/components/utils/categoriasSectionLp3.js
@@ -4,6 +4,24 @@ import img3c1Section3Lp from '../../assets/images/landing/section3/categoria1/fo
 import img4c1Section3Lp from '../../assets/images/landing/section3/categoria1/fondo-1-categoria-landing-3.4.webp';
 import img5c1Section3Lp from '../../assets/images/landing/section3/categoria1/fondo-1-categoria-landing-3.5.webp';
 
+import img1c2Section3Lp from '../../assets/images/landing/section3/categoria2/fondo-2-categoria-landing-3.1.webp';
+import img2c2Section3Lp from '../../assets/images/landing/section3/categoria2/fondo-2-categoria-landing-3.2.webp';
+import img3c2Section3Lp from '../../assets/images/landing/section3/categoria2/fondo-2-categoria-landing-3.3.webp';
+import img4c2Section3Lp from '../../assets/images/landing/section3/categoria2/fondo-2-categoria-landing-3.4.webp';
+import img5c2Section3Lp from '../../assets/images/landing/section3/categoria2/fondo-2-categoria-landing-3.5.webp';
+
+import img1c3Section3Lp from '../../assets/images/landing/section3/categoria3/fondo-3-categoria-landing-3.1.webp';
+import img2c3Section3Lp from '../../assets/images/landing/section3/categoria3/fondo-3-categoria-landing-3.2.webp';
+import img3c3Section3Lp from '../../assets/images/landing/section3/categoria3/fondo-3-categoria-landing-3.3.webp';
+import img4c3Section3Lp from '../../assets/images/landing/section3/categoria3/fondo-3-categoria-landing-3.4.webp';
+import img5c3Section3Lp from '../../assets/images/landing/section3/categoria3/fondo-3-categoria-landing-3.5.webp';
+
+import img1c4Section3Lp from '../../assets/images/landing/section3/categoria4/fondo-4-categoria-landing-3.1.webp';
+import img2c4Section3Lp from '../../assets/images/landing/section3/categoria4/fondo-4-categoria-landing-3.2.webp';
+import img3c4Section3Lp from '../../assets/images/landing/section3/categoria4/fondo-4-categoria-landing-3.3.webp';
+import img4c4Section3Lp from '../../assets/images/landing/section3/categoria4/fondo-4-categoria-landing-3.4.webp';
+import img5c4Section3Lp from '../../assets/images/landing/section3/categoria4/fondo-4-categoria-landing-3.5.webp';
+
 export const categoriasSectionLp3 = [
     {
         nombre: 'Newborn',
@@ -24,11 +42,11 @@ export const categoriasSectionLp3 = [
         ruta: '/servicios/ninos',
         alt: 'Servicio de retratos Niños: Por que siempre seran nuestros Bebés.',
         imagenes: {
-            default: img1c1Section3Lp,
-            xs: img2c1Section3Lp,
-            sm: img3c1Section3Lp,
-            md: img4c1Section3Lp,
-            xl: img5c1Section3Lp,
+            default: img1c2Section3Lp,
+            xs: img2c2Section3Lp,
+            sm: img3c2Section3Lp,
+            md: img4c2Section3Lp,
+            xl: img5c2Section3Lp,
         },
     },
     {
@@ -37,11 +55,11 @@ export const categoriasSectionLp3 = [
         ruta: '/servicios/embarazo',
         alt: 'Servicio de retratos Embarazo: Inmortaliza la belleza y emoción de la dulce espera.',
         imagenes: {
-            default: img1c1Section3Lp,
-            xs: img2c1Section3Lp,
-            sm: img3c1Section3Lp,
-            md: img4c1Section3Lp,
-            xl: img5c1Section3Lp,
+            default: img1c3Section3Lp,
+            xs: img2c3Section3Lp,
+            sm: img3c3Section3Lp,
+            md: img4c3Section3Lp,
+            xl: img5c3Section3Lp,
         },
     },
     {
@@ -50,11 +68,11 @@ export const categoriasSectionLp3 = [
         ruta: '/servicios/familia',
         alt: 'Servicio de retratos Familia: Conexión, calidez y amor que trasciende',
         imagenes: {
-            default: img1c1Section3Lp,
-            xs: img2c1Section3Lp,
-            sm: img3c1Section3Lp,
-            md: img4c1Section3Lp,
-            xl: img5c1Section3Lp,
+            default: img1c4Section3Lp,
+            xs: img2c4Section3Lp,
+            sm: img3c4Section3Lp,
+            md: img4c4Section3Lp,
+            xl: img5c4Section3Lp,
         },
     },
 ];
