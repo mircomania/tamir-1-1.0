800668cb92aed9b12842e38d2d22532afe006ed9
mircomania <mirco.rodriguez@hotmail.com>
2025-05-14 16:16:20 -0400

    40 tamir commit - full imagenes galeria page

--- src/components/utils/categoriasSectionGp1.js
+++ src/components/utils/categoriasSectionGp1.js
@@ -1,7 +1,22 @@
-import img1c1Section1Gp from '../../assets/images/galeria/categoria1/img-1-galerias-1.1.webp';
-import img2c1Section1Gp from '../../assets/images/galeria/categoria1/img-1-galerias-1.2.webp';
-import img3c1Section1Gp from '../../assets/images/galeria/categoria1/img-1-galerias-1.3.webp';
-import img4c1Section1Gp from '../../assets/images/galeria/categoria1/img-1-galerias-1.4.webp';
+import img1c1Section1Gp from '../../assets/images/galeria/cate1/img-1-galerias-1.webp';
+import img2c1Section1Gp from '../../assets/images/galeria/cate1/img-1-galerias-2.webp';
+import img3c1Section1Gp from '../../assets/images/galeria/cate1/img-1-galerias-3.webp';
+import img4c1Section1Gp from '../../assets/images/galeria/cate1/img-1-galerias-4.webp';
+
+import img1c2Section1Gp from '../../assets/images/galeria/cate2/img-2-galerias-1.webp';
+import img2c2Section1Gp from '../../assets/images/galeria/cate2/img-2-galerias-2.webp';
+import img3c2Section1Gp from '../../assets/images/galeria/cate2/img-2-galerias-3.webp';
+import img4c2Section1Gp from '../../assets/images/galeria/cate2/img-2-galerias-4.webp';
+
+import img1c3Section1Gp from '../../assets/images/galeria/cate3/img-3-galerias-1.webp';
+import img2c3Section1Gp from '../../assets/images/galeria/cate3/img-3-galerias-2.webp';
+import img3c3Section1Gp from '../../assets/images/galeria/cate3/img-3-galerias-3.webp';
+import img4c3Section1Gp from '../../assets/images/galeria/cate3/img-3-galerias-4.webp';
+
+import img1c4Section1Gp from '../../assets/images/galeria/cate4/img-4-galerias-1.webp';
+import img2c4Section1Gp from '../../assets/images/galeria/cate4/img-4-galerias-2.webp';
+import img3c4Section1Gp from '../../assets/images/galeria/cate4/img-4-galerias-3.webp';
+import img4c4Section1Gp from '../../assets/images/galeria/cate4/img-4-galerias-4.webp';
 
 export const categoriasSectionGp1 = [
     {
@@ -22,12 +37,12 @@ export const categoriasSectionGp1 = [
         id: 'niños',
         alt: 'Galería de Sesiones Niños: Que sean nuestros niños por siempre.',
         imagenes: {
-            default: img1c1Section1Gp,
-            xs: img2c1Section1Gp,
-            sm: img1c1Section1Gp,
-            md: img2c1Section1Gp,
-            lg: img3c1Section1Gp,
-            xl: img4c1Section1Gp,
+            default: img1c2Section1Gp,
+            xs: img2c2Section1Gp,
+            sm: img1c2Section1Gp,
+            md: img2c2Section1Gp,
+            lg: img3c2Section1Gp,
+            xl: img4c2Section1Gp,
         },
     },
     {
@@ -35,12 +50,12 @@ export const categoriasSectionGp1 = [
         id: 'embarazo',
         alt: 'Galería de Sesiones Embarazo: La belleza y calidez esperando su llegada.',
         imagenes: {
-            default: img1c1Section1Gp,
-            xs: img2c1Section1Gp,
-            sm: img1c1Section1Gp,
-            md: img2c1Section1Gp,
-            lg: img3c1Section1Gp,
-            xl: img4c1Section1Gp,
+            default: img1c3Section1Gp,
+            xs: img2c3Section1Gp,
+            sm: img1c3Section1Gp,
+            md: img2c3Section1Gp,
+            lg: img3c3Section1Gp,
+            xl: img4c3Section1Gp,
         },
     },
     {
@@ -48,12 +63,12 @@ export const categoriasSectionGp1 = [
         id: 'familia',
         alt: 'Galería de Sesiones Familia: Complicidad y amor incondicional',
         imagenes: {
-            default: img1c1Section1Gp,
-            xs: img2c1Section1Gp,
-            sm: img1c1Section1Gp,
-            md: img2c1Section1Gp,
-            lg: img3c1Section1Gp,
-            xl: img4c1Section1Gp,
+            default: img1c4Section1Gp,
+            xs: img2c4Section1Gp,
+            sm: img1c4Section1Gp,
+            md: img2c4Section1Gp,
+            lg: img3c4Section1Gp,
+            xl: img4c4Section1Gp,
         },
     },
 ];
